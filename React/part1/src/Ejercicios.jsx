6645338bd1fc34db73b7dfff8053fd3039989576
kevin15042005 <kevin2013118@gmail.com>
2025-02-09 20:47:49 -0500

    Se hacen cambios de Ejercicios

--- React/part1/src/Ejercicios.jsx
+++ React/part1/src/Ejercicios.jsx
@@ -2,7 +2,7 @@ const Ejercicios = () => {
     return (
       <div>
         <h1>Bienvenidos a la página de Ejercicios</h1>
-        <p>Aquí practicaremos los ejercicios</p>
+        <p>Aquí practicaremos los ejercicios de React</p>
       </div>
     );
   };
