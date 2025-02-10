1b8498a7cf3f45bbbb2872b9e088e92b29000648
kevin15042005 <kevin2013118@gmail.com>
2025-02-09 21:01:30 -0500

    Sequita el segundo parrafo

--- React/part1/src/Ejercicios.jsx
+++ React/part1/src/Ejercicios.jsx
@@ -3,7 +3,6 @@ const Ejercicios = () => {
       <div>
         <h1>Bienvenidos a la página de Ejercicios</h1>
         <p>Aquí practicaremos los ejercicios de React</p>
-        <p>Darmeos inicio a esto</p>
       </div>
     );
   };
