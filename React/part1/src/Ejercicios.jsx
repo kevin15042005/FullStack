a693fa2894a171aa1886992dfa34e8b42ae73907
kevin15042005 <kevin2013118@gmail.com>
2025-02-09 20:50:01 -0500

    Se hace la adacion de un parrafo

--- React/part1/src/Ejercicios.jsx
+++ React/part1/src/Ejercicios.jsx
@@ -3,6 +3,7 @@ const Ejercicios = () => {
       <div>
         <h1>Bienvenidos a la página de Ejercicios</h1>
         <p>Aquí practicaremos los ejercicios de React</p>
+        <p>Darmeos inicio a esto</p>
       </div>
     );
   };
